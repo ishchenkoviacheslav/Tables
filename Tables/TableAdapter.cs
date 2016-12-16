c0b4f1dc46a67cef81b7148e5dca56ad9187dae7
user <user@DESKTOP-05J4HLG>
2016-12-16 11:26:35 +0200

    :)

--- Tables/TableAdapter.cs
+++ Tables/TableAdapter.cs
@@ -32,10 +32,10 @@ namespace Tables
             dAdapt.Fill(inv);
             return inv;
         }
-//        Здесь объект адаптера данных проверяет значение RowState у каждой строки
-//входной таблицы DataTable.В зависимости от его значения(т.е.RowState.Added,
-//RowState.Deleted или RowState.Modified) автоматически используется нужный
-//объект команды.
+        //        Здесь объект адаптера данных проверяет значение RowState у каждой строки
+        //входной таблицы DataTable.В зависимости от его значения(т.е.RowState.Added,
+        //RowState.Deleted или RowState.Modified) автоматически используется нужный
+        //объект команды.
 
         public void UpdateContact(DataTable modifiedTable)
         {
